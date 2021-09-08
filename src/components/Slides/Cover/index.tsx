10dc11374ba37fd79db4548cb6ef0498514a5102
Ana Tarrisse <at@demodern.de>
2021-09-08 19:58:26 -0300

    feat: sroll cover image

--- src/components/Slides/Cover/index.tsx
+++ src/components/Slides/Cover/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Ratio from "react-ratio";
+import debounce from "lodash/debounce";
+import React, { useState } from "react";
 
 import Image from "@/components/Image";
 import Slide from "@/components/Slides/Slide";
@@ -7,6 +7,12 @@ import Slide from "@/components/Slides/Slide";
 import * as styles from "./styles.module.scss";
 
 const Cover: React.FC = () => {
+  const [isAtBeginning, setIsAtBeginning] = useState(true);
+
+  const handleScroll = (e) => {
+    setIsAtBeginning(e.target.scrollLeft === 0);
+  };
+
   return (
     <Slide id="cover">
       <div className={styles.cover}>
@@ -16,15 +22,25 @@ const Cover: React.FC = () => {
           2020
         </h1>
         <Image filename="logo.png" alt="Logo do Doutores da Alegria" className={styles.logo} height={96} />
-        <Ratio ratio={4098 / 2115} className={styles.bg}>
-          <Image
-            alt="Foto em estilo de colagem mostrando os rostos de diversos colaboradores do Doutores da Alegria"
-            filename="cover_desktop.png"
-            className={styles.logo} height={96} />
-        </Ratio>
+      </div>
+      <div>
+        <div className={styles.wrapper}>
+          <div className={styles.gradient} data-begin={isAtBeginning}>
+            <div className={styles.scroll} onScroll={debounce(handleScroll, 10)}>
+              <div className={styles.content}>
+                <Image
+                  alt="Foto em estilo de colagem mostrando os rostos de diversos colaboradores do Doutores da Alegria"
+                  filename="cover_desktop.png"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </Slide>
   );
 };
 
 export default Cover;
+
+
