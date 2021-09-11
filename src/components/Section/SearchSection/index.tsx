bebaf71e154460687a718f90c59fb9c0339fb8e7
Ana Tarrisse <at@demodern.de>
2021-09-10 22:15:40 -0300

    feat: seção PF colapsável

--- src/components/Section/SearchSection/index.tsx
+++ src/components/Section/SearchSection/index.tsx
@@ -1,5 +1,5 @@
 import { graphql, useStaticQuery } from "gatsby";
-import React from "react";
+import React, { useRef } from "react";
 import { useEffect, useState } from "react";
 import AnimateHeight from "react-animate-height";
 
@@ -10,10 +10,13 @@ import useWindowSize from "@/utils/useWindowSize";
 
 import * as styles from "./styles.module.scss";
 
+const HEIGHT = 500;
+
 const SearchSection = () => {
   const { isMobile } = useWindowSize();
   const donors = queryDonors().map(donor => donor.node["Doador"]);
   const half = Math.ceil(donors.length / 2);
+  const slide = document.querySelector("#parcerias-e-apoios-34");
 
   const [focus, setFocus] = useState(false);
   const [empty, setEmpty] = useState(true);
@@ -22,7 +25,7 @@ const SearchSection = () => {
   const [showAll, setShowAll] = useState(false);
   const [firstHalf, setFirstHalf] = useState(donors.slice(0, half));
   const [lastHalf, setLastHalf] = useState(donors.slice(-half));
-  const [height, setHeight] = useState<string | number>(200);
+  const [height, setHeight] = useState<string | number>(HEIGHT);
 
 
   const handleChange = e => {
@@ -37,7 +40,10 @@ const SearchSection = () => {
   };
 
   useEffect(() => {
-    setHeight(showAll ? "auto" : 200);
+    setHeight(showAll ? "auto" : HEIGHT);
+    if (!showAll) slide?.scrollIntoView({
+      behavior: "smooth",
+    })
   }, [showAll]);
 
   useEffect(() => {
@@ -82,29 +88,27 @@ const SearchSection = () => {
         <>
           <AnimateHeight
             className={styles.panel}
-            id="names-columns"
+            data-expanded={showAll}
             duration={500}
+            id="names-columns"
             height={height} // see props documentation below
           >
-            <div className={styles.names} data-show-all={showAll}>
+            <div className={styles.names}>
               <ul className={styles.list}>
                 {firstHalf.map((donor) => <li key={donor} className={styles.item}>{donor}</li>)}
               </ul>
-              <ul className={styles.list} data-show-all={showAll}>
+              <ul className={styles.list}>
                 {lastHalf.map((donor) => <li key={donor} className={styles.item}>{donor}</li>)}
               </ul>
             </div>
           </AnimateHeight>
           <button
-            aria-expanded={height !== 0}
+            aria-expanded={height > HEIGHT}
             aria-controls="names-columns"
             className={styles.button}
             onClick={toggleNames}
           >
-            {
-              showAll ? "Esconder nomes" : "Veja todos os nomes"
-            }
-
+            {showAll ? "Esconder nomes" : `Veja todos os ${donors.length} nomes`}
           </button>
         </>
       }
