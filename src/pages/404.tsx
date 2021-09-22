575e82c14cd7f5c7db1db5c518ed626b940ef047
Ana Tarrisse <at@demodern.de>
2021-09-22 20:36:24 -0300

    feat: add 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,24 @@
+import Image from "@/components/Image";
+import React from "react";
+import * as styles from "./styles.module.scss";
+
+type TFourOuFour = React.HTMLAttributes<HTMLDivElement>;
+
+type Props = {
+  className?: string;
+}
+
+const FourOuFour: React.FC<Props & TFourOuFour> = () => {
+  return (
+    <div className={styles.fourOuFour}>
+      <h1>Erro 404!</h1>
+      <p>Iiiiii deu erro!</p>
+      <a href="https://balanco.doutoresdaalegria.org.br/">
+        <p>Mas só clicar aqui para voltar à tela inicial</p>
+        <Image filename="404.png" alt="palhaço com uma mão acima dos olhos, como quem busca por algo perdido." />
+      </a>
+    </div>
+  )
+}
+
+export default FourOuFour
